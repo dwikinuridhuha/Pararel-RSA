d25c0e1645a5eb014c061b240b06cede2e30a427
dwiki (HeartBreaking) <dwiki.nuridhuha@gmail.com>
2018-12-07 19:09:42 +0700

    Signed-off-by: dwiki (HeartBreaking) <dwiki.nuridhuha@gmail.com>
    siap implementasi ke RSAnya

--- test_bro.py
+++ test_bro.py
@@ -29,31 +29,24 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-# QN = int(input("masukan Nilai QN: "))
-QN = 121
+QN = 120
 a = []
 
 for x in range(QN):
 	a.append(x)
 
-# for x in range(QN):
-# 	print(a[x])
-
 cgg = cekGanjilGenap(QN, size)
 
 if("genap" == cgg):
 	n = QN/size
-	atas = rank*n
-	bawah = ((rank+1)*n)-1
+	atas = int(rank*n)
+	bawah = int(((rank+1)*n)-1)
 	print("rank: ", rank, "atas: ", atas, "bawa: ", bawah)
-	for x in range(int(rank*(QN/size)), int(((rank+1)*(QN/size)))):
-		print("rank :", rank, "a: ", a[x])
 else:
 	n = QN/size
 	atas = int(rank*n)
 	bawah = int(((rank+1)*n)-1)
 	print("rank: ", rank, "atas: ", atas, "bawa: ", bawah)
 
-print("\n")
-
-# print("size: ", size, "dan rank: ", rank, " QN: ", QN, 'CGG: ', cgg, "\n")
+for x in range(atas, bawah+1):
+	print("rank : ", rank, "a: ", a[x])
